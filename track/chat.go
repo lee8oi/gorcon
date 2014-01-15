32a9c8bcd5071d915b088ae4201f85798d2d5eff
Lee Forest <lee8oi@gmail.com>
2014-01-14 20:35:33 -0500

    Added check & clear methods. Updated parse method.d. Renamed new to add.

--- track/chat.go
+++ track/chat.go
@@ -13,7 +13,7 @@ package track
 
 import (
 	"fmt"
-	"html"
+	//"html"
 	"strings"
 )
 
@@ -25,7 +25,8 @@ type chat struct {
 	messages []message
 }
 
-func (c *chat) new(data string) {
+//add takes 'bf2cc clientchatbuffer' string and appends all messages to chat.
+func (c *chat) add(data string) {
 	if len(data) > 1 {
 		split := strings.Split(data, "\r")
 		for _, value := range split {
@@ -35,7 +36,7 @@ func (c *chat) new(data string) {
 			} else if len(elem) < 6 {
 				elem = append(elem, " ")
 			} else {
-				elem[5] = strings.Replace(html.EscapeString(elem[5]), `\`, `\\`, -1)
+				//elem[5] = strings.Replace(html.EscapeString(elem[5]), `\`, `\\`, -1)
 			}
 			m := message{
 				Origin: elem[1],
@@ -50,11 +51,31 @@ func (c *chat) new(data string) {
 	return
 }
 
-//parse existing messages then clear chat.
-func (c *chat) parse() {
-	var base []message
-	for key, value := range c.messages {
-		fmt.Println(key, value)
+//parse existing chat messages. Returns slice containing any command lines found.
+func (c *chat) parse() (cmdlist []string) {
+	for _, value := range c.messages {
+		cmd := c.check(value)
+		if len(cmd) > 0 {
+			cmdlist = append(cmdlist, cmd)
+		}
+		fmt.Println(value)
 	}
+	return
+}
+
+//check message for command prefixes then return command line.
+func (c *chat) check(value message) (cmd string) {
+	if len(value.Text) > 1 {
+		trimd := strings.TrimSpace(strings.TrimPrefix(value.Text, ": ")) //for testing commands via admin chat
+		if strings.IndexAny(trimd, "!/|") == 0 {
+			cmd = trimd[1:]
+		}
+	}
+	return
+}
+
+//clear all chat messages
+func (c *chat) clear() {
+	var base []message
 	c.messages = base
 }
