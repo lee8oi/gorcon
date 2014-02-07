c5c86009d6fd020325b2790b61038275cd30be07
Lee Forest <lee8oi@gmail.com>
2014-02-06 19:43:06 -0500

    Updated comment documentation and added README.

---
+++ log/README.md
@@ -0,0 +1,8 @@
+gorcon/log - Experimental
+======
+
+Source code for this package is based on the code by Gary Burd found at
+http://gary.beagledreams.com/page/go-websocket-chat.html
+
+The log package is used to log messages to the web via gorilla websocket. This
+version of the package is intended to be used with gorcon.
--- log/main.go
+++ log/main.go
@@ -1,3 +1,19 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+gorcon/log (lee8oi)
+
+Source code for this package is based on the code by Gary Burd found at
+http://gary.beagledreams.com/page/go-websocket-chat.html
+
+*/
+
+/*
+The log package is used to log messages to the web via gorilla websocket. This
+version of the package is intended to be used with gorcon.
+*/
 package log
 
 import (
